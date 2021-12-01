6631d37ef81eea03de28a2d5699ae96b23f063f6
UnaidedShip2769 <76848947+UnaidedShip2769@users.noreply.github.com>
2021-12-01 15:49:08 +0200

    Added new datasets and implemented an alternative way of reading files. Also new object oritented apporach
    This is mostly a test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <iterator>
+#include <fstream>
+#include <vector>
 
-int main() {
-    std::cout << "3" << std::endl;
+using namespace std;
+
+class document
+{
+public:
+    vector<string> contents;
+    vector<int> hash;
+
+    document()
+    {
+        bool flag = false;
+        while(!flag)
+        {
+            string path;
+            cout << "Enter path for dataset (eg ../datasets/text1.txt): ";
+            cin >> path;
+            string line;
+            ifstream file(path);
+            if (file.good())
+            {
+                flag = true;
+                while (getline(file, line)) {
+                    this->contents.push_back(line);
+                }
+            }
+            else
+            {
+                cout << "File not found! Please enter a valid path!" << endl;
+            }
+        }
+    }
+    void print()
+    {
+        for (string i : this->contents)
+        {
+           cout << i << endl;
+        }
+    };
+};
+
+int main()
+{
+    document doc1;
+    doc1.print();
+
     return 0;
 }
