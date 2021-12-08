2b77aecdc638bbb22426966d7785e17299efda50
PeriKor19008 <peri1908kor@gmail.com>
2021-12-08 17:03:27 +0200

    kd-tree complete untested

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <set>
 #include "LSH.hpp"
+#include "KDtree.hpp"
 
 
 int main() {
@@ -18,7 +19,16 @@ int main() {
     Merge(s2,vocub);
     onehot1= onehot_encode(s1,vocub);
     vector<vector<int>>min;
-    min= minhash(onehot1,3);
+    min= minhash(onehot1,2);
+
+
+    node* tree1=NULL;
+    for (int i=0;i<min.size();i++){
+        tree1= insert(tree1,min.at(i));
+    }
+
+
+
 
     return 0;
 }//planning
